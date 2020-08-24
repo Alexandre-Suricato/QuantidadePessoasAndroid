8dea834ab94806cae7bb5083c578805c989dc528
alexandre.cardoso <Ale300694>
2020-08-24 16:09:24 -0300

    Criado o design system

---
+++ app/src/main/java/br/com/zup/quantidadepessoas/AppDesignSystem.kt
@@ -0,0 +1,16 @@
+package br.com.zup.quantidadepessoas
+
+import br.com.zup.beagle.android.annotation.BeagleComponent
+import br.com.zup.beagle.android.setup.DesignSystem
+
+@BeagleComponent
+class AppDesignSystem : DesignSystem() {
+
+    override fun image(id: String): Int? =
+        when (id){
+            "background" -> R.drawable.background
+
+        else -> super.image(id)
+    }
+
+}
